3972e1a5c2cadf3e4d155fc6138677a4813f1839
Diego <diegolopesdld@gmail.com>
2022-07-29 21:02:44 -0300

    add profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import * as Icon from "react-icons/fa";
+import "./profile.css";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div className="profile-container">
+      <div className="profile-content">
+        <p className="image">
+          <Icon.FaUserCircle size={80} />
+        </p>
+        <p>
+          <b>Diego Lopes</b>
+        </p>
+        <p>
+          <b>E-mail: </b> diegolopesdld@gmail.com
+        </p>
+        <button>Alterar dados</button>
+      </div>
+    </div>
+  );
 }
