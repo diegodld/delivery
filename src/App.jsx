bfe621116cfa87c38afd72f4a8963ef372d4ec4b
Diego <diegolopesdld@gmail.com>
2022-07-29 21:05:54 -0300

    header name

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Navigation from "./components/navigation/Navigation";
 import "./App.css";
 import AppRoutes from "./Routes";
-import Header from "./components/header/header";
+import Header from "./components/header/Header";
 
 function App() {
   return (
